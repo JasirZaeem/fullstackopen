3c56e1a7ce1f1665943c3613513b3d141e70ea29
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2020-11-27 18:24:06 +0530

    Part 3c mongo.js
    Exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password and/or contact details as arguments: node mongo.js <password>, [<person>, <number>]"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullso:${password}@cluster2-ostce.mongodb.net/test?retryWrites=true`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach(({ name, number }) => {
+      console.log(`${name} ${number}`);
+    });
+    mongoose.connection.close();
+  });
+}
