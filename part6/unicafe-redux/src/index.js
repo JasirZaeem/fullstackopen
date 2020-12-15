344cbd4b782d3a9b719ae7f7f450cbad3a36a3f7
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2020-12-15 16:44:11 +0530

    Part 6a
    Exercises 6.1-6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,57 +1,63 @@
-import React, {useState} from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import reducer from "./reducer";
 
-
-const Statistic = ({text, value}) => {
+const Statistic = ({ text, value }) => {
   return (
     <tr>
       <td>{text}</td>
       <td>{value}</td>
     </tr>
-  )
-}
+  );
+};
 
-const Statistics = ({good, bad, neutral}) => {
-  let total = good + bad + neutral
+const Statistics = ({ good, bad, neutral }) => {
+  let total = good + bad + neutral;
   return (
     <div>
       <h1>statistics</h1>
-      {
-        total ?
-          <table>
-            <tbody>
-              <Statistic text={"good"} value={good}/>
-              <Statistic text={"neutral"} value={neutral}/>
-              <Statistic text={"bad"} value={bad}/>
-              <Statistic text={"all"} value={total}/>
-              <Statistic text={"average"} value={(good - bad) / (total)}/>
-              <Statistic text={"positive"} value={`${good * 100 / (total)} %`}/>
-            </tbody>
-          </table>
-          : "No feedback given"
-      }
+      {total ? (
+        <table>
+          <tbody>
+            <Statistic text={"good"} value={good} />
+            <Statistic text={"neutral"} value={neutral} />
+            <Statistic text={"bad"} value={bad} />
+            <Statistic text={"all"} value={total} />
+            <Statistic text={"average"} value={(good - bad) / total} />
+            <Statistic text={"positive"} value={`${(good * 100) / total} %`} />
+          </tbody>
+        </table>
+      ) : (
+        "No feedback given"
+      )}
     </div>
-  )
-}
+  );
+};
+
+const store = createStore(reducer);
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  // destructure out values
+  const { good, neutral, bad } = store.getState();
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={() => store.dispatch({ type: "GOOD" })}>good</button>
+      <button onClick={() => store.dispatch({ type: "NEUTRAL" })}>
+        neutral
+      </button>
+      <button onClick={() => store.dispatch({ type: "BAD" })}>bad</button>
 
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App/>,
-  document.getElementById('root')
-)
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
+
+renderApp();
+store.subscribe(renderApp);
